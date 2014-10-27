c57affedc05dabac0adf1f49b8153b7e9f97c8b0
Alec <alec.sokso@icloud.com>
2014-10-27 17:47:41 -0400

    Revert "Removed session dependency for deleting list items"
    This reverts commit aac89135b8b2b7a76156f7b4e2ebf513e024531f.

--- delete_item.php
+++ delete_item.php
@@ -8,7 +8,7 @@
 	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 	$uid = $_SESSION['id'];
 	$listitle = $_SESSION['title'];
-	$lid = $_GET['lid'];
+	$lid = $_SESSION['lid'];
 	$eid = $_GET['submit'];
 	$eid = strip_tags($eid);
 	$eid = mysqli_real_escape_string($dbCon, $eid);
--- user.php
+++ user.php
@@ -19,6 +19,11 @@ if (isset($_SESSION['url'])){
 		unset($_SESSION['url']);
 	}
 }
+if (isset($_SESSION['lid'])){
+	if($_SESSION['lid'] != ''){
+		unset($_SESSION['lid']);
+	}
+}
 if (isset($_SESSION['item'])){
 	if($_SESSION['item'] != ''){
 		unset($_SESSION['item']);
