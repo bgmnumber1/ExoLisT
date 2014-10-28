d411aff26ebe2745058e917cef72a78c58cef8d3
Alec <alec.sokso@icloud.com>
2014-10-28 14:46:18 -0400

    Revert 83c0be0..786e059
    This rolls back to commit 83c0be00e2afcb621781926bc3c99cb055aabd9d.

--- listview.php
+++ listview.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['id'])) {
 				<title>ExoLisT - ERROR</title>	
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
-				<link rel="stylesheet" href="includes/jquery.mobile-1.4.2.min.css">
+				<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
 				<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 				<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
 			</head>
@@ -100,7 +100,7 @@ $title = get_listitle($lid, $dbCon);
 	<title>ExoLisT-<?php echo $usname ;?></title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="includes/jquery.mobile-1.4.2.min.css">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
 	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
 </head>
@@ -156,7 +156,7 @@ $title = get_listitle($lid, $dbCon);
 								?>
 								
 							</td>
-							<td style="white-space: normal">
+							<td>
 								<?php echo $row['content']; ?>
 							</td>
 							<td>
